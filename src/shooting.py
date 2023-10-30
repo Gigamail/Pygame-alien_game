d29490e32476706cf8384550b30fafa22c9d41a2
Gigamail <vidhanpoddar@gmail.com>
2023-10-30 21:18:28 +0530

    moving the object

--- src/shooting.py
+++ src/shooting.py
@@ -2,16 +2,26 @@ import pygame,sys
 pygame.init()
 screen=pygame.display.set_mode((640,520))
 pygame.display.set_caption("Alien Invasion")
-myspace=pygame.image.load("C:\\Users\\vidha\\Desktop\\Mini Projects\\pygame\\images\\ship_shield.png").convert()
+myspace=pygame.image.load("C:\\Users\\vidha\\Desktop\\Mini Projects\\pygame\\images\\ship.png").convert()
+pygame.display.set_icon(myspace)
 pygame.display.flip()
+x=280
+y=465
 running=True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             sys.exit()
-    for i in range(1):
-        screen.blit(myspace,(280,465))
-
+    keys=pygame.key.get_pressed()
+    if keys[pygame.K_DOWN]:
+        y+=1
+    if keys[pygame.K_UP]:
+        y-=1
+    if keys[pygame.K_LEFT]:
+        x-=1
+    if keys[pygame.K_RIGHT]:
+        x+=1
+    screen.blit(myspace,(x,y))
     pygame.display.update()
 pygame.quit()
